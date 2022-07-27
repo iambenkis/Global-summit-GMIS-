2e511d8015b3babadfcaae24d89999c3c7260fb2
benjamin kisenge <benkisenge03@gmail.com>
2022-07-27 12:49:33 +0200

    Create speaker Object

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+const speakers = [
+    {
+        name : 'Jonh Maxwell',
+        title : 'Leadership Expert',
+        description : `Maxwell bachelor's degree at Circleville Bible College in 1969`,
+        image : './imgs/jonhmaxwell.png'
+    },
+    {
+        name : 'llllllll',
+        title : 'Leadership Expert',
+        description : `Maxwell bachelor's degree at Circleville Bible College in 1969`,
+        image : './imgs/jonhmaxwell.png'
+    } ,
+    {
+        name : 'vvvvvvvvvv',
+        title : 'Leadership Expert',
+        description : `Maxwell bachelor's degree at Circleville Bible College in 1969`,
+        image : './imgs/jonhmaxwell.png'
+    }
+];
 const hambuger = document.querySelector('.fa-bars');
 const mobileMenu = document.querySelector('.mobile-menu');
 const faMark = document.querySelector('.fa-xmark');
 const lists = document.querySelectorAll('.na-list');
+const speakerApp = document.querySelector('.speaker-container');
 
 const openMenu = () => {
     mobileMenu.classList.add('active-menu');
@@ -16,3 +37,17 @@ faMark.addEventListener('click', closeMenu);
 lists.forEach(list => {
     list.addEventListener('click',closeMenu);
 })
+
+speakers.forEach((item) => {
+    speakerApp.innerHTML += `
+    <div class="speaker">
+        <img src=${item.image} alt="">
+        <div class="speaker-id">
+            <h3>${item.name}</h3>
+            <h4 id="job">${item.title}</h4>
+            <p>${item.description}</p>
+        </div>
+    </div>
+`
+})
+
