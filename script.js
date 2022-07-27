2b6218adf0fd9b108ac502c6e6e857083c9130c9
benjamin kisenge <benkisenge03@gmail.com>
2022-07-27 12:08:54 +0200

    Add script for menu openning

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const hambuger = document.querySelector('.fa-bars');
 const mobileMenu = document.querySelector('.mobile-menu');
+const faMark = document.querySelector('.fa-xmark');
+const lists = document.querySelectorAll('.na-list');
 
 const openMenu = () => {
     mobileMenu.classList.add('active-menu');
 }
 
+const closeMenu = () => {
+    mobileMenu.classList.remove('active-menu');
+}
+
 hambuger.addEventListener('click', openMenu);
+faMark.addEventListener('click', closeMenu);
+lists.forEach(list => {
+    list.addEventListener('click',closeMenu);
+})
